5a7f810c30051a18e0b14efbeb8e6f115c2adea7
Nurzhanov Oleg <olegnastyle@yandex.ru>
2024-10-17 21:21:04 +0300

    update

--- client/app.vue
+++ client/app.vue
@@ -2,6 +2,7 @@
   <NuxtLoadingIndicator throttle="0" />
   <Navbar />
   <NuxtPage />
+  <Up />
   <Footer />
 </template>
 
@@ -18,30 +19,9 @@
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: base+config.favicon.url },
-    ]
+    ],
+    bodyAttrs: {
+      class: 'container mx-auto'
+    },
   })
 </script>
-
-<style>
-/* * {
-  margin: 0;
-} */
-
-html, body, #__nuxt {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-}
-
-body {
-  margin: 0;
-  /* background: linear-gradient(180deg, #00000025 0%, #000000 100%), url("https://i0.wp.com/www.funnyart.club/uploads/posts/2022-01/1641464486_63-www-funnyart-club-p-abstraktnie-sine-golubie-foni-63.jpg?ssl=1");
-  background-blend-mode: darken; */
-}
-
-main {
-  flex: 1 1 auto;
-}
-
-</style>
