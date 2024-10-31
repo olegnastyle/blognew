e0eecb7ef9dd448a82376fcb5c4968b7c0491d45
Oleg Nurzhanov <oleg@nastyle.pro>
2024-10-31 20:19:49 +0300

    update: http://76a67cc8ae3c.vps.myjino.ru

--- client/pages/category/[id].vue
+++ client/pages/category/[id].vue
@@ -27,9 +27,9 @@
 
 
 <script setup>
-  const base_url = "http://localhost:1337"
+  const base_url = "http://76a67cc8ae3c.vps.myjino.ru"
   const { id } = useRoute().params
-  const api = await $fetch(`http://localhost:1337/api/categories/${id}?populate=posts.img&populate=posts.categories`)
+  const api = await $fetch(`http://76a67cc8ae3c.vps.myjino.ru/api/categories/${id}?populate=posts.img&populate=posts.categories`)
   const posts = api.data.posts
   const displayedPosts = ref(posts.slice(0, 12)) // отображаем первые 12 статей
   const loadMore = () => displayedPosts.value = posts.slice(0, displayedPosts.value.length + 4) // добавляем еще 4
