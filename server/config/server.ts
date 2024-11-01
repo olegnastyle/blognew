58be070eb3c690012b9fb63521c4b64e275ff17f
Oleg Nurzhanov <oleg@nastyle.pro>
2024-11-02 01:32:52 +0300

    update

--- server/config/server.ts
+++ server/config/server.ts
@@ -2,9 +2,9 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   // url: 'https://825f3fb1e2d0.vps.myjino.ru/api',
-  // admin: {
-  //   url: 'https://825f3fb1e2d0.vps.myjino.ru/admin',
-  // },
+  admin: {
+    url: 'https://825f3fb1e2d0.vps.myjino.ru/admin',
+  },
   app: {  
     keys: env.array('APP_KEYS'),
   },
