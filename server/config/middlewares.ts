f62d7496d5bab7b12d01f0d448907df2348ee778
Oleg Nurzhanov <oleg@nastyle.pro>
2024-11-02 01:14:31 +0300

    update

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -2,16 +2,16 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  //'strapi::cors',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['https://825f3fb1e2d0.vps.myjino.ru'], // Укажите разрешенные источники
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Разрешенные методы
-      allowedHeaders: ['Content-Type', 'Authorization'], // Разрешенные заголовки
-      credentials: true, // Разрешить отправку куки
-    },
-  },
+  'strapi::cors',
+  // {
+  //   name: 'strapi::cors',
+  //   config: {
+  //     origin: ['https://825f3fb1e2d0.vps.myjino.ru'], // Укажите разрешенные источники
+  //     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Разрешенные методы
+  //     allowedHeaders: ['Content-Type', 'Authorization'], // Разрешенные заголовки
+  //     credentials: true, // Разрешить отправку куки
+  //   },
+  // },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
