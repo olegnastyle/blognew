1a50fcdeae64d79669637097e26f91e81464cec8
Oleg Nurzhanov <oleg@nastyle.pro>
2024-11-01 23:34:11 +0300

    update

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -4,6 +4,12 @@ module.exports = {
             name: 'server-dev',
             script: 'yarn',
             args: 'develop',
+            env: {
+                NODE_ENV: 'development',
+                ADMIN_PATH: '/admin',
+                STRAPI_ADMIN_BACKEND_URL: 'http://localhost:1337/admin',
+                STRAPI_TELEMETRY_DISABLED: 'true',
+            },
         },
     ],
 };
