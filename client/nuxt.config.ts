4a87c5cc33f768608eb54e456671034c5ccc50ed
Oleg Nurzhanov <oleg@nastyle.pro>
2024-11-02 01:52:32 +0300

    update

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devServer: {
-    host: '0.0.0.0',
+    host: 'localhost',
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
