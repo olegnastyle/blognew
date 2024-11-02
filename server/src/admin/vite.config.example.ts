abebb7349353a5cd17ba8795c0a4fc51bd378eda
Oleg Nurzhanov <oleg@nastyle.pro>
2024-11-02 12:55:58 +0300

    update

--- server/src/admin/vite.config.example.ts
+++ server/src/admin/vite.config.example.ts
@@ -1,12 +1,22 @@
 import { mergeConfig, type UserConfig } from 'vite';
 
 export default (config: UserConfig) => {
-  // Important: always return the modified config
+  // Важно: всегда возвращайте изменённую конфигурацию
   return mergeConfig(config, {
     resolve: {
       alias: {
-        '@': '/src',
+        '@': '/src', // Установка алиаса для пути к src
       },
     },
+    server: {
+      proxy: {
+        // Настройка прокси для API Strapi
+        '/api': {
+          target: 'https://825f3fb1e2d0.vps.myjino.ru', // Убедитесь, что это базовый URL
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(/^\/api/, '') // Удаляем '/api' из пути
+        }
+      }
+    }
   });
 };
